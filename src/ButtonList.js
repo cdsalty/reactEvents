8fb8360068336d2f1b620a6e5ea2599a1fab631b
cdsalty <soltiscd@gmail.com>
2019-06-03 15:02:10 -0400

    passing methods change button colors

--- src/ButtonList.js
+++ src/ButtonList.js
@@ -5,12 +5,21 @@ class ButtonList extends Component {
   static defaultProps = {
       colors: ["#e056fd", "#eb4d4b", "#badc58", "#f0932b"]
   };
+  constructor(props){
+    super(props);
+    this.state = { color: "cyan"}
+  }
+  changeColor(newColor){
+    console.log(`newColor is: ${newColor}`);
+    this.setState({ color: newColor });
+  }
+
   render(){
     return(
-      <div className='ButtonList'>
+      <div className='ButtonList' style={{backgroundColor: this.state.color}}>
         {this.props.colors.map(c => {
           const colorObject = { backgroundColor: c };
-          return <button style={colorObject}>Click On Me!</button>;
+          return <button style={colorObject} onClick={this.changeColor.bind(this, c)}>Click On Me!</button>;
         })}
       </div>
     );
